c6ddaf2fea4e10152c3735e13361cb53874ce2e4
shinhash <71681922+shinhash@users.noreply.github.com>
2022-03-12 15:46:39 +0900

    hash commit 2022-03-12 test commiut

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>test react project</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>hash shin test react project</h2>
     </div>
   );
 }
